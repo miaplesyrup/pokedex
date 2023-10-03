370b49a02f9fb646b98d284da1969baeb4d24d7e
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-10-03 22:04:09 +0800

    pokemon

--- pokedex.py
+++ pokedex.py
@@ -1,9 +1,21 @@
 import requests
+import sys
 
 def search_pokemon(name):
-    # response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
-    response = requests.get("https://pokeapi.co/api/v2/pokemon/charmander")
-    print(response.json())
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
+    # response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format{name})
+    # print(response.json())
+
+    pokemon = response.json()
+    # print(pokemon["name"])
+    # print(pokemon["id"])
+
+    print(f"Name: {pokemon['name'].capitalize()}")
+    print(f"ID: {pokemon['id']}")
+    print(f"Base XP: {pokemon['base_experience']}")
+
+
+
 
 if __name__ == "__main__":
-    search_pokemon("charmander")
+    search_pokemon(sys.argv[1])
