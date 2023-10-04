71caec62e7a1f7077e91aea89169b591425398c9
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-10-04 18:24:42 +0800

    with frontend

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def search_pokemon(name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}/"
+    response = requests.get(url)
+    
+    if response.status_code == 404:
+        return None
+    
+    pokemon_data = response.json()
+    
+    return {
+        "name": pokemon_data.get('name', '').capitalize(),
+        "id": pokemon_data.get('id', ''),
+        "hp": pokemon_data.get('stats', [{}])[0].get('base_stat', ''),
+        "attacks": [move.get('move', {}).get('name', '') for move in pokemon_data.get('moves', [])],
+        "held_items": [item.get('item', {}).get('name', '') for item in pokemon_data.get('held_items', [])],
+        "image_url": pokemon_data.get('sprites', {}).get('front_default', ''),
+    }
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    pokemon_data = None
+    
+    if request.method == "POST":
+        pokemon_name = request.form["pokemon_name"].lower()
+        pokemon_data = search_pokemon(pokemon_name)
+    
+    return render_template("index.html", pokemon_data=pokemon_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- pokemon_challenge3.py
+++ pokemon_challenge3.py
@@ -1,3 +1,39 @@
+# import requests
+# import sys
+
+# def search_pokemon(name):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
+
+#     pokemon = response.json()
+
+#     if response.status_code == 200:
+#         pokemon = response.json()
+#         if 'stats' in pokemon and 'moves' in pokemon:
+#             hp = pokemon['stats'][0]['base_stat']
+#             print(f"HP: {hp}")
+#             attacks = [move['move']['name'] for move in pokemon['moves']]
+#             print(f"Attacks: {', '.join(attacks)}")
+#         else:
+#             print("HP and Attacks data not found for this Pokémon.")
+        
+#         # Check if 'held_items' is present in the response JSON
+#         if 'held_items' in pokemon:
+#             # Print Held Items
+#             held_items = [item['item']['name'] for item in pokemon['held_items']]
+#             print(f"Held Items: {', '.join(held_items)}")
+#         else:
+#             print("Held Items data not found for this Pokémon.")
+#     else:
+#         print("Pokemon not found!")
+
+
+
+# if __name__ == "__main__":
+#     search_pokemon(sys.argv[1])
+
+
+
+
 import requests
 import sys
 
@@ -5,36 +41,40 @@ def search_pokemon(name):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
 
     pokemon = response.json()
+    print(pokemon)
 
-    if response.status_code == 200:
-        pokemon = response.json()
-        
-        # Print Name
-        print(f"Name: {pokemon['name'].capitalize()}")
-        
-        # Check if 'stats' and 'moves' are present in the response JSON
-        if 'stats' in pokemon and 'moves' in pokemon:
-            # Print HP
-            hp = pokemon['stats'][0]['base_stat']
-            print(f"HP: {hp}")
-            
-            # Print Attacks
-            attacks = [move['move']['name'] for move in pokemon['moves']]
-            print(f"Attacks: {', '.join(attacks)}")
-        else:
-            print("HP and Attacks data not found for this Pokémon.")
-        
-        # Check if 'held_items' is present in the response JSON
-        if 'held_items' in pokemon:
-            # Print Held Items
-            held_items = [item['item']['name'] for item in pokemon['held_items']]
-            print(f"Held Items: {', '.join(held_items)}")
-        else:
-            print("Held Items data not found for this Pokémon.")
-    else:
-        print("Pokemon not found!")
+    name = pokemon.get('name', '').capitalize()
+    print(f"Name: {name}")
+    print(f"Name: {pokemon['name'].capitalize()}")
+    print(f"ID: {pokemon['id']}")
+
+    hp = pokemon.get('stats', [{}])[0].get('base_stat', '')
+    print(f"HP: {hp}")
+    print(f"HP: {pokemon['stats'][0]['base_stat']}")
+    
+
+    attacks = [] 
+    for move in pokemon.get('moves', []):
+        move_name = move.get('move', {}).get('name', '')
+        attacks.append(move_name)
+    print(f"Attacks: {', '.join(attacks)}")
+
+    held_items = []
+    for item in pokemon.get('held_items', []):
+        item_name = item.get('item', {}).get('name', '')
+        held_items.append(item_name)
+    print(f"Held items: {', '.join(held_items)}")
 
 
+
+def main():
+    while True:
+        pokemon_name = input("Enter a Pokemon name (or 'exit' to quit): ").lower()
+
+        if pokemon_name == 'exit':
+            break
+
+        search_pokemon(pokemon_name)
 
 if __name__ == "__main__":
-    search_pokemon(sys.argv[1])
+    main()
