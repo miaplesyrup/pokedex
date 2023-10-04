ffeac1c658103a07e4c60ffc3ea6a3066a3f076c
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-10-04 18:31:33 +0800

    added docstrings

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+# pylint: disable=trailing-whitespace
+"""
+you need a docstring
+"""
 from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
+"""kelangan ulit ng docstring"""
 def search_pokemon(name):
+    """doctring ulit"""
     url = f"https://pokeapi.co/api/v2/pokemon/{name}/"
     response = requests.get(url)
-    
     if response.status_code == 404:
-        return None
-    
+        return None  
     pokemon_data = response.json()
     
     return {
@@ -17,12 +21,14 @@ def search_pokemon(name):
         "id": pokemon_data.get('id', ''),
         "hp": pokemon_data.get('stats', [{}])[0].get('base_stat', ''),
         "attacks": [move.get('move', {}).get('name', '') for move in pokemon_data.get('moves', [])],
-        "held_items": [item.get('item', {}).get('name', '') for item in pokemon_data.get('held_items', [])],
+        "held_items": [item.get('item', {}).get('name', '') 
+        for item in pokemon_data.get('held_items', [])],
         "image_url": pokemon_data.get('sprites', {}).get('front_default', ''),
     }
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """docstring ulit"""
     pokemon_data = None
     
     if request.method == "POST":
@@ -33,4 +39,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+    
